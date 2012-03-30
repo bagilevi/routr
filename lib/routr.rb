5f8c289361c5ed349d457e07dc6056111b862415
Levente Bagi <bagilevi@gmail.com>
2012-03-30 18:58:33 +0100

    autoload Route and Hop

--- lib/routr.rb
+++ lib/routr.rb
@@ -2,6 +2,8 @@ require "routr/version"
 
 module Routr
   autoload :Interface, 'routr/interface'
+  autoload :Route, 'routr/interface'
+  autoload :Hop, 'routr/interface'
 
   class << self
     def new(*args, &block)
